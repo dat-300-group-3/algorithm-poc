1acae2b50ae982714a48b0dab40bd489f991fe86
mlinus@chalmers.se <mlinus@chalmers.se>
2024-10-03 16:24:57 +0200

    linus testing

--- test.py
+++ test.py
@@ -1,45 +1,12 @@
 import redis
-import time
-import csv
-from datetime import date
-
 
 # Connect to Redis
 r = redis.Redis()
 
-def is_allowed(row):
-    today = date.today()
+#Get the top 5 IPs
+elements = r.topk().list('ip_device:24h', True)
+paired_elements = list(zip(elements[::2], elements[1::2]))
 
-    current_time = int(time.time())
-    print("Date:",today)
-    key = f"rate_limit:{today}"  # Generate a Redis key
-    
-    pipe = r.pipeline()
-    
-    #window size is one hour
-    pipe.zremrangebyscore(key, 0, current_time - 3600)
-    pipe.zadd(key, {"%d-%s-%s-%s" %(current_time, row['ip'], row['device'], row['channel']): current_time})
-    pipe.zcard(key)
-    #expire in one minute
-    pipe.expire(key, 60)
-    
-    # Execute the pipeline
-    action_countv = pipe.execute()
-    print(action_countv)
-    action_count = action_countv[2]
-
-    limit = 5
-
-    # Check if the user has exceeded the limit
-    if action_count <= limit:
-        return True  # Action is allowed
-    else:
-        return False  # Rate limit exceeded
-
-with open('data/sorted_file.csv', 'r') as csv_file:
-            reader = csv.DictReader(csv_file)
-            for row in reader:
-                if is_allowed(row):
-                     print("Action allowed")
-                else:
-                    print("Rate limit exceeded")
+# Print the top 5 IPs
+for tuple in paired_elements:
+    print(f"IP: {tuple[0]}, Count: {tuple[1]}")
